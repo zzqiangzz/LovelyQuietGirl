0de77d2b209bb2a7dd1710461113458203eecf49
Jackpeng <757858105@qq.com>
2021-02-01 15:50:06 +0800

    bugly接入成功

--- app/src/main/java/com/jackpeng/lqg/MainActivity.java
+++ app/src/main/java/com/jackpeng/lqg/MainActivity.java
@@ -3,6 +3,12 @@ package com.jackpeng.lqg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvUpgradeInfo = findViewById(R.id.tv_hello);
     }
+
+    public void onClick(View view){
+        int idV = view.getId();
+        if (idV == R.id.tv_hello){
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+            Beta.checkAppUpgrade(true, false);
+        } else if (idV == R.id.press_it){
+            loadUpgradeInfo();
+        }
+    }
+
+
+
+    private TextView tvUpgradeInfo;
+    private void loadUpgradeInfo() {
+        if (tvUpgradeInfo == null)
+            return;
+
+        /***** 获取升级信息 *****/
+        UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
+
+        if (upgradeInfo == null) {
+            tvUpgradeInfo.setText("无升级信息");
+            return;
+        }
+
+        StringBuilder info = new StringBuilder();
+        info.append("id: ").append(upgradeInfo.id).append("\n");
+        info.append("标题: ").append(upgradeInfo.title).append("\n");
+        info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n");
+        info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n");
+        info.append("versionName: ").append(upgradeInfo.versionName).append("\n");
+        info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n");
+        info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n");
+        info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n");
+        info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n");
+        info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n");
+        info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n");
+        info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n");
+        info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType);
+
+        tvUpgradeInfo.setText(info);
+    }
+
 }
--- app/src/main/java/com/jackpeng/lqg/MyApplication.java
+++ app/src/main/java/com/jackpeng/lqg/MyApplication.java
@@ -1,13 +1,16 @@
 package com.jackpeng.lqg;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.tencent.bugly.Bugly;
 
 public class MyApplication extends Application {
+    private final String TAG = MyApplication.class.getSimpleName();
     @Override
     public void onCreate() {
         super.onCreate();
-        Bugly.init(getApplicationContext(), "83c8cd1434", false);
+        Log.d(TAG,"init finish");
+        Bugly.init(getApplicationContext(), "83c8cd1434", true);
     }
 }
